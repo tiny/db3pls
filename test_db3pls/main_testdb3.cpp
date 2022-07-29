968fe84eda13d83f73c45a7fa6eef8796635a56d
Robert McInnis <tiny@solidice.com>
2022-07-29 13:07:44 -0400

    removed worthless dbs

--- test_db3pls/main_testdb3.cpp
+++ test_db3pls/main_testdb3.cpp
@@ -29,15 +29,22 @@ void db_dump( const char *name )
 //  int16_t  iWholesale = db.get_field_no("WHOLESALE");
 
   uint32_t  n_displayed = 25;
+  uint16_t  sz = 0;
   n_displayed = std::min(n_displayed, db.nRecords());
 
   for (int i = 0; i < n_displayed; i++)
   {
-    db.get_field( 0, b1);
-    db.get_field( 1, b2);
-    db.get_field( 2, b3);
-    db.get_field( 3, b4);
-    printf("%6d.  %s %s %s %s\n", i, b1, b2, b3, b4 );
+    printf("%6d.  ", i);
+    sz = 0;
+    for (int j = 0; j < db.nFields(); j++)
+    {
+      db.get_field(j, b1);
+      sz += strlen(b1);
+      if (sz > 200) 
+        break;
+      printf("%s ", b1);
+    }
+    printf("\n");
 
     db.goto_next();
   }
@@ -47,9 +54,7 @@ void db_dump( const char *name )
 
 int main()
 {
-  db_dump("company");
   db_dump("idl");
-  db_dump("location");
   db_dump("product");
   db_dump("zipcodes");
   return 0;
